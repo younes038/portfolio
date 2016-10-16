02e5958e97f5938d88442e7a6074d9770c8a9a39
younes <Younes.El-Yandouzi@etu.univ-grenoble-alpes.fr>
2016-10-17 01:39:19 +0200

    Correct wrong paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
 	console.log("hello");
-	gulp.src('sass/**/*.scss')
+	gulp.src('./assets/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./assets/css/'));
 });
 
-gulp.task('default', function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+gulp.task('default', function () {
+    gulp.watch('.assets/sass/**/*.scss', ['styles']);
 });
