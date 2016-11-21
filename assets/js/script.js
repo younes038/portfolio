fab1d6e96f29ead58e74aa59d37c5bb8b3c4cc9e
younes038 <younes.el-yandouzi@etu.univ-grenoble-alpes.fr>
2016-11-21 18:09:14 +0100

    Made nav menu for mobile with scroll handle & nav button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,40 @@
-$(document).ready(function () {
-	$('#menu-icon').on('click', function () {
-		if ($("#menu-icon").css("right").val() === "0%") {
-			$("#menu-icon").css("right", "100%");
+// Fires automatically on DOM ready
+(function() {
+
+	function displayNavMobile(e) {
+		var target = document.querySelector('ul');
+
+		target.classList.toggle('is-open');
+	}
+
+	function toggleNav() {
+		// Defining targets
+		var button = document.querySelector('#menu-icon');
+		var btnClose = document.querySelector('#menu-close');
+
+		// click event
+		if ( button ) {
+			button.addEventListener('click', displayNavMobile, false);
+			btnClose.addEventListener('click', displayNavMobile, false);
+			$('nav ul').each(function() {
+					$("li a", this).each(function() {
+						this.addEventListener('click', displayNavMobile, false);
+				});
+			});
+		}
+	}
+
+	toggleNav();
+
+
+	$( document ).scroll(function() {
+		var nav = $('nav');
+		var origOffsetY = $('header').height() - $('nav').height();
+		
+		if ($(window).scrollTop() >= origOffsetY) {
+			nav.addClass('sticky');
 		} else {
-			$("#menu-icon").css("right", "0%");
+			nav.removeClass('sticky');
 		}
 	});
-});
+}());
